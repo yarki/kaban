fce06895f4be6240a3b02a637068b36dd3c2c841
Yar Ki <yarki@users.noreply.github.com>
2014-08-04 22:07:41 +0300

    Unlimited number of items to fetch

--- kaban/fetchers/jira_rest.rb
+++ kaban/fetchers/jira_rest.rb
@@ -3,33 +3,36 @@ def fetch(collection: collection, endpoint: endpoint)
 	query = collection.fetcher.query
 	puts "query: #{query}"
 	options = {
-	            :username => endpoint.username,
-	            :password => endpoint.password,
-	            :site     => endpoint.site,
-	            :context_path => endpoint.context_path,
-	            :auth_type => endpoint.auth_type,
-	            :use_ssl => endpoint.use_ssl
-	          }
+		:username => endpoint.username,
+		:password => endpoint.password,
+		:site     => endpoint.site,
+		:context_path => endpoint.context_path,
+		:auth_type => endpoint.auth_type,
+		:use_ssl => endpoint.use_ssl
+	}
 	client = JIRA::Client.new(options)
 	puts 'fetching...'
 	output = []
-	max_size = 1500
 	chunk_size = 100
-	iterations = max_size / chunk_size
-	(1..iterations).each { |i|
-		start_at = (i - 1) * chunk_size
-		puts 'chunk %02d: %03d-%03d' % [i, start_at, start_at + chunk_size - 1]
+	start_at = 0
+	i = 0
+	loop {
+		start_at = i * chunk_size
+		puts 'chunk %03d: %04d-%04d' % [i, start_at, start_at + chunk_size - 1]
+		result = []
 		Retriable.retriable :on => JIRA::HTTPError, :tries => 3, :interval => 10 do
-			client.Issue.jql(query, nil, start_at, chunk_size).map { |issue|
+			result = client.Issue.jql(query, nil, start_at, chunk_size).map { |issue|
 				doc = {}
 				doc['key'] = issue.key
 				doc['id'] = issue.id
 				doc['fields'] = issue.fields
 				doc
-			}.each { |doc|
-				output << doc
 			}
 		end
+		break if result.empty?
+		output = output.concat(result)
+		break if result.length < chunk_size
+		i += 1
 	}
 	puts "fetched #{output.length} docs"
 	output
