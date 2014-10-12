8806751541c4ebae95dbffa8939ee34ae269ecf0
Yar Ki <yaraslau.kiryllau@gmail.com>
2014-10-12 17:33:06 +0300

    rubocop warnings fixed

--- Rakefile
+++ Rakefile
@@ -2,8 +2,8 @@ task default: 'test:hello'
 
 namespace :test do
 
-	task :hello do
-		puts "Hello!"
-	end
+  task :hello do
+    puts 'Hello!'
+  end
 
 end
--- kaban.rb
+++ kaban.rb
@@ -17,64 +17,69 @@ require_relative 'kaban/core/transformer'
 $stdout.sync = true
 
 configatron.configure_from_hash(
-	YAML.load_file('./kaban/config.yml').merge({'env' => YAML.load_file('./repository/environment.yml')})
+  YAML.load_file('./kaban/config.yml').merge(
+    env: YAML.load_file('./repository/environment.yml')
+  )
 )
 
+# Main class
 class Kaban < Thor
-	
-	desc 'config', 'print configuration'
-	def config
-		puts configatron.inspect
-	end
+  desc 'config', 'print configuration'
+  def config
+    puts configatron.inspect
+  end
 
-	desc 'reset <collection>', 'reset collections'
-	def reset(collection_name = nil)
-		Repository.collections(name: collection_name).each { |collection|
-			schema = Repository.load_schema name: collection.indexer.schema
-			Indexer.reset collection: collection, schema: schema
-		}
-	end
+  desc 'reset <collection>', 'reset collections'
+  def reset(collection_name = nil)
+    Repository.collections(name: collection_name).each do
+      |collection|
+      schema = Repository.load_schema name: collection.indexer.schema
+      Indexer.reset collection: collection, schema: schema
+    end
+  end
 
-	desc 'fetch <collection>', 'fetch raw data'
-	def fetch(collection_name = nil)
-		Repository.collections(name: collection_name).each { |collection|
-			docs = Synchronizer.fetch collection: collection
-			Workspace.save_raw docs: docs, collection: collection
-		}
-	end
+  desc 'fetch <collection>', 'fetch raw data'
+  def fetch(collection_name = nil)
+    Repository.collections(name: collection_name).each do
+      |collection|
+      docs = Synchronizer.fetch collection: collection
+      Workspace.save_raw docs: docs, collection: collection
+    end
+  end
 
-	desc 'map <collection>', 'apply mapping'
-	def map(collection_name = nil)
-		Repository.collections(name: collection_name).each { |collection|
-			raw_docs = Workspace.load_raw collection: collection
-			mapping = Repository.load_mapping name: collection.mapper.mapping
-			mapped_docs = Transformer.map collection: collection, raw_docs: raw_docs, mapping: mapping
-			Workspace.save_mapped collection: collection, docs: mapped_docs  
-		}
-	end
+  desc 'map <collection>', 'apply mapping'
+  def map(collection_name = nil)
+    Repository.collections(name: collection_name).each do
+      |collection|
+      raw_docs = Workspace.load_raw collection: collection
+      mapping = Repository.load_mapping name: collection.mapper.mapping
+      mapped_docs = Transformer.map collection: collection,
+        raw_docs: raw_docs, mapping: mapping
+      Workspace.save_mapped collection: collection, docs: mapped_docs
+    end
+  end
 
-	desc 'index <collection>', 'index all'
-	def index(collection_name = nil)
-		Repository.collections(name: collection_name).each { |collection|
-			docs = Workspace.load_mapped collection: collection
-			schema = Repository.load_schema name: collection.indexer.schema
-			Indexer.index collection: collection, docs: docs, schema: schema 
-		}
-	end
+  desc 'index <collection>', 'index all'
+  def index(collection_name = nil)
+    Repository.collections(name: collection_name).each do
+      |collection|
+      docs = Workspace.load_mapped collection: collection
+      schema = Repository.load_schema name: collection.indexer.schema
+      Indexer.index collection: collection, docs: docs, schema: schema
+    end
+  end
 
-	desc 'init', 'init kaban repository'
-	def init
-		
-	end
+  desc 'init', 'init kaban repository'
+  def init
+  end
 
-	desc 'sync <collection>', 'fetch + map + reset + index'
-	def sync(collection_name = nil)
-		fetch collection_name
-		map collection_name
-		reset collection_name
-		index collection_name
-	end
-
+  desc 'sync <collection>', 'fetch + map + reset + index'
+  def sync(collection_name = nil)
+    fetch collection_name
+    map collection_name
+    reset collection_name
+    index collection_name
+  end
 end
 
 Kaban.start(ARGV)
